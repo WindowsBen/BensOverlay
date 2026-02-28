8f8ae2720b8d1ffac481610da78c1d8094371566
Ben <windowswindowsben@gmail.com>
2026-02-28 20:03:18 +0100

    wiwiwi

--- src/ui/paints.js
+++ src/ui/paints.js
@@ -18,6 +18,7 @@ function applyPaint(element, paint) {
     // Build the gradient or solid color for -webkit-background-clip text effect
     const gradientCSS = buildPaintGradient(paint);
     if (gradientCSS) {
+        element.style.display            = 'inline-block';
         element.style.backgroundImage    = gradientCSS;
         element.style.webkitBackgroundClip = 'text';
         element.style.backgroundClip     = 'text';
@@ -27,7 +28,6 @@ function applyPaint(element, paint) {
 }
 
 function buildPaintGradient(paint) {
-    console.log('[7TV Paint] buildPaintGradient received:', JSON.stringify(paint));
     const stops = (paint.stops || []).map(s => {
         return `${intToRGBA(s.color)} ${(s.at * 100).toFixed(1)}%`;
     });
